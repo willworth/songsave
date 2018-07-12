b150262d1f813f33eba004e2aad143a8525e5f11
Will Worth <will.worth@gmail.com>
2018-07-12 18:06:39 +0200

    remote db added

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const song = require('./routes/routes'); // Imports routes for the songs
 
-
-
 // initialize 
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://southgate:football1@ds235711.mlab.com:35711/songsave';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.use('/songs', song);
 
@@ -14,11 +26,5 @@ let port = 2323;
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
 });
-
-
-
-
-
-
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
-# clear crud
+# song save
 
+This is my node express mongo react crud project.  
 
 
 https://medium.com/@es1amaged/stop-using-created-at-with-mongodb-a9d03e6b5385
@@ -7,5 +8,4 @@ https://medium.com/@es1amaged/stop-using-created-at-with-mongodb-a9d03e6b5385
 http://mongoosejs.com/docs/middleware.html
 
 http://mongoosejs.com/docs/guide.html
-
 
