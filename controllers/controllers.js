23ef0f22dc36c5c2aa9200a95284a0174380e84c
Will Worth <will.worth@gmail.com>
2018-07-12 19:20:14 +0200

    add create db entry

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -4,3 +4,22 @@ const Song = require('../models/models');
 exports.test = function (req, res) {
     res.send('Greetings from the new song test controller!');
 };
+
+
+
+exports.create = function (req, res) {
+    let song = new Song(
+        {
+            title: req.body.title,
+            artist: req.body.artist,
+            body: req.body.body
+        }
+    );
+
+    song.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Song Created successfully')
+    })
+};
--- routes/routes.js
+++ routes/routes.js
@@ -8,4 +8,11 @@ const song_controller = require('../controllers/controllers');
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', song_controller.test);
 
+
+
+router.post('/create', song_controller.create);
+
+
+
+
 module.exports = router;
